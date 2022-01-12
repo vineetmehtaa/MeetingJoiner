ecaf75bd27d78b3aa4ef2ba8a4b9ce6bfa99ecf5
vineetmehtaa <83648521+vineetmehtaa@users.noreply.github.com>
2022-01-12 22:20:03 +0530

    Update pull.py

--- pull.py
+++ pull.py
@@ -3,12 +3,16 @@ import validators
 from setup import setup
 from sys import argv, exit
 
+# importing details from "details.txt" to validate the import of information
 config = open("details.txt", "r")
 check = config.readlines()
 config.close()
 
+# pulling details from the repo for timetable as well as links
 if len(check) == 3 and check[0][-6:-4] == "PU" and validators.url(check[1]) and validators.url(check[2]):
 	pull_details = requests.get("https://raw.githubusercontent.com/vineetmehtaa/MeetingJoiner/main/pull.txt")
 	exec(pull_details.text)
+
+# sending user to instantiate setup for "details.txt"
 else:
 	setup()
