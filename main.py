63b97726b1d460043d78aff1e0a52a03843ced4b
vineetmehtaa <83648521+vineetmehtaa@users.noreply.github.com>
2022-01-12 22:18:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from setup import setup
 from sys import argv, exit
 import validators
 
+# importing file contents for estimating function calls
 config = open("details.txt", "r")
 check = config.readlines()
 config.close()
 
+# checking if content in "details.txt" is valid for function calls
 if len(check) == 3 and check[0][-6:-4] == "PU" and validators.url(check[1]) and validators.url(check[2]):
 	findLinkAndLaunch()
 
+# instantiate setup process for "details.txt"
 else:
 	input("Enter any key to start setup...")
 	setup()
