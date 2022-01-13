506ce295a1cf6a8ac14ef9c1cb0727d5b097ae69
Vineet Mehta <83648521+vineetmehtaa@users.noreply.github.com>
2022-01-13 20:27:33 +0530

    Update pull.py
    changed comment detail

--- pull.py
+++ pull.py
@@ -8,7 +8,7 @@ config = open("details.txt", "r")
 check = config.readlines()
 config.close()
 
-# pulling details from the repo for timetable as well as links
+# pulling details from the repo for timetable as well as links from "details.txt"
 if len(check) == 3 and check[0][-6:-4] == "PU" and validators.url(check[1]) and validators.url(check[2]):
 	pull_details = requests.get("https://raw.githubusercontent.com/vineetmehtaa/MeetingJoiner/main/pull.txt")
 	exec(pull_details.text)
