1868d6f37780067bc197749ee2306e2e6cbe2fa8
Vineet Mehta <83648521+vineetmehtaa@users.noreply.github.com>
2022-01-15 03:57:59 +0530

    Update pull.py

--- pull.py
+++ pull.py
@@ -3,6 +3,15 @@ import validators
 from setup import setup
 from sys import argv, exit
 
+# checking for internet connection
+url = "https://www.google.com/"
+timeout = 5
+try:
+	request = requests.get(url, timeout=timeout)
+except (requests.ConnectionError, requests.Timeout) as exception:
+	input("No internet connection! Try again!")
+	exit()
+
 # importing details from "details.txt" to validate the import of information
 config = open("details.txt", "r")
 check = config.readlines()
